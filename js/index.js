a7022995a87320893f8c7e9031599f5329a72453
shayonasamson <ssamson7@nait.ca>
2024-04-12 21:19:38 -0600

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const apiURL = "https://660ded886ddfa2943b3573c8.mockapi.io/api/v1/albums";
+async function appInit() {
+  const res = await fetch(apiURL);
+  const payload = await res.json();
+}
+
+appInit();
